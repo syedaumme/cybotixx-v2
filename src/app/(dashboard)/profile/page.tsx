6cb425d7d2c281d0218ba9bc9693444bc7984a27
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2024-11-15 14:40:56 +0530

    modified:   src/app/(dashboard)/profile/page.tsx

--- src/app/(dashboard)/profile/page.tsx
+++ src/app/(dashboard)/profile/page.tsx
@@ -3,32 +3,23 @@ import React from "react";
 import { useGetUserByClerkId } from "@/features/users/api/use-get-user";
 import { useAuth } from "@clerk/nextjs";
 import ProfileForm from "./_components/profile-form";
-import { LoaderIcon } from "lucide-react";
 import { AnimatePresence, motion } from "framer-motion";
 const ProfilePage = () => {
   const { userId } = useAuth();
 
-  const { data, isLoading } = useGetUserByClerkId({
+  const { data } = useGetUserByClerkId({
     clerkId: userId as string,
   });
-
-  if (isLoading) {
-    return (
-      <div className="h-screen flex justify-center items-center">
-        <LoaderIcon className="size-10 text-green-500 animate-spin" />
-      </div>
-    );
-  }
 
   if (!data) return;
 
   return (
     <AnimatePresence mode="wait">
       <motion.div
-        initial={{ opacity: 0, x: 20 }}
-        animate={{ opacity: 1, x: 0 }}
-        exit={{ opacity: 0, x: -20 }}
-        transition={{ duration: 0.1 }}
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: -50 }}
+        transition={{ duration: 0.2 }}
       >
         <div className="w-full flex justify-center items-center">
           <ProfileForm
